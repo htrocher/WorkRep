9151d8da48554dfefa93c19a4d325dd26a46769e
hugotroche <hugotroche@MacBook-Air.local>
2023-09-17 16:02:14 -0500

    test files

---
+++ hellofriend.py
@@ -0,0 +1,8 @@
+
+print("hello world")
+print("what is your name, my friend")
+myname = input()
+print("welcome dear " + myname)
+print("what is your age")
+myage = input()
+print("you will be " + str(int(myage)+1) + " in a year")
